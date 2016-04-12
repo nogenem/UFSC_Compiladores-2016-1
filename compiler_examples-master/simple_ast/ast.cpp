4853a6c15ee22679c9697b99b93cbae306027926
gilney nathanael mathias <gilney_salvo@hotmail.com>
2016-04-12 13:54:16 -0300

    Cópia do código do professor
    Cópia do código do professor do link:
    https://github.com/llpilla/compiler_examples

---
+++ compiler_examples-master/simple_ast/ast.cpp
@@ -0,0 +1,71 @@
+#include "ast.h"
+#include "st.h"
+
+using namespace AST;
+
+extern ST::SymbolTable symtab;
+
+/* Print methods */
+void Integer::printTree(){
+    std::cout << value;
+    return;
+}
+
+void BinOp::printTree(){
+    left->printTree();
+    switch(op){
+        case plus: std::cout << " + "; break;
+        case times: std::cout << " * "; break;
+        case assign: std::cout << " = "; break;
+    }
+    right->printTree();
+    return;
+}
+
+void Block::printTree(){
+    for (Node* line: lines) {
+        line->printTree();
+        std::cout << std::endl;
+    }
+}
+
+void Variable::printTree(){
+    if (next != NULL){
+        next->printTree();
+        std::cout << ", ";
+    }
+    std::cout << id;
+}
+
+/* Compute methods */
+int Integer::computeTree(){
+    return value;
+}
+
+int BinOp::computeTree(){
+    int value, lvalue, rvalue;
+    lvalue = left->computeTree();
+    rvalue = right->computeTree();
+    switch(op){
+        case plus: value = lvalue + rvalue; break;
+        case times: value = lvalue * rvalue; break;
+        case assign:
+            Variable* leftvar = dynamic_cast<Variable*>(left);
+            symtab.entryList[leftvar->id].value = rvalue;
+            value = rvalue;
+    }
+    return value;
+}
+
+int Block::computeTree(){
+    int value;
+    for (Node* line: lines) {
+        value = line->computeTree();
+         std::cout << "Computed " << value << std::endl;
+    }
+    return 0;
+}
+
+int Variable::computeTree(){
+    return symtab.entryList[id].value;
+}
